e79b686e42b0e9c0478186a4247952cd284666b3
Leandro Tabares Martin <ltmartin1987@gmail.com>
2023-02-15 16:36:59 +0100

    Removing warnings.

--- WebSocket/WebSocketEcho/src/main/java/EchoServer.java
+++ WebSocket/WebSocketEcho/src/main/java/EchoServer.java
@@ -1,9 +1,11 @@
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.server.ServerEndpoint;
 
+@SuppressWarnings("unused")
 @ServerEndpoint("/echo")
 public class EchoServer {
 
+    @SuppressWarnings("unused")
     @OnMessage
     public String echo(String message){
         return "I have received this (" + message + ") message";

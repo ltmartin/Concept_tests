b0bffaa535f4e91effb690ccdff5dc435b6fcec1
Leandro Tabares Martin <ltmartin1987@gmail.com>
2023-03-01 15:50:12 +0100

    deleteEmployeeById method annotated as Transactional.

--- FullStack/SpringBootAngular/EmployeeManager/src/main/java/be/ltmartin/employeemanager/repo/EmployeeRepo.java
+++ FullStack/SpringBootAngular/EmployeeManager/src/main/java/be/ltmartin/employeemanager/repo/EmployeeRepo.java
@@ -2,11 +2,13 @@ package be.ltmartin.employeemanager.repo;
 
 import be.ltmartin.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
     Optional<Employee> findEmployeeById(Long id);
 
+    @Transactional
     void deleteEmployeeById(Long id);
 }

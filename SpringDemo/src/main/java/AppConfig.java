ad7019b662deb768f5aed324d821830688d55a19
Leandro Tabares Martín <ltmartin1987@gmail.com>
2023-05-19 10:57:14 +0200

    Fully autowired application.

--- SpringDemo/src/main/java/AppConfig.java
+++ SpringDemo/src/main/java/AppConfig.java
@@ -1,12 +1,5 @@
-import be.ltmartin.springdemo.repository.HibernateSpeakerRepositoryImpl;
-import be.ltmartin.springdemo.repository.SpeakerRepository;
-import be.ltmartin.springdemo.service.SpeakerService;
-import be.ltmartin.springdemo.service.SpeakerServiceImpl;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan({"be.ltmartin.springdemo"})
--- SpringDemo/src/main/java/be/ltmartin/springdemo/service/SpeakerService.java
+++ SpringDemo/src/main/java/be/ltmartin/springdemo/service/SpeakerService.java
@@ -5,6 +5,6 @@ import be.ltmartin.springdemo.model.Speaker;
 import java.util.List;
 
 public interface SpeakerService {
-    public static final String SPEAKER_SERVICE_BEAN_NAME = "speakerService";
+    String SPEAKER_SERVICE_BEAN_NAME = "speakerService";
     List<Speaker> findAll();
 }

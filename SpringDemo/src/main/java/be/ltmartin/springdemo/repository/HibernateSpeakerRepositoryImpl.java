8fe99e1e4d116c39c299241417fb400fe5f64a3c
Leandro Tabares Martín <ltmartin1987@gmail.com>
2023-05-19 11:34:34 +0200

    Playing with FactoryBean

--- SpringDemo/src/main/java/be/ltmartin/springdemo/repository/HibernateSpeakerRepositoryImpl.java
+++ SpringDemo/src/main/java/be/ltmartin/springdemo/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,22 @@
 package be.ltmartin.springdemo.repository;
 
 import be.ltmartin.springdemo.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository(SpeakerRepository.SPEAKER_REPOSITORY_BEAN_NAME)
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+    private final Calendar cal;
+
+    @Autowired
+    public HibernateSpeakerRepositoryImpl(Calendar cal) {
+        this.cal = cal;
+    }
+
     @Override
     public List<Speaker> findAll(){
         List<Speaker> speakers = new ArrayList<>();
@@ -16,6 +25,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstName("Leandro");
         speaker.setLastName("Tabares-Martín");
         speakers.add(speaker);
+
+        System.out.println(cal.getTime());
 
         return speakers;
     }

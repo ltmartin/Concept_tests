1729329b83154a8edf05373c03c999b3dfcbe579
Leandro Tabares Martín <ltmartin1987@gmail.com>
2023-05-22 11:07:08 +0200

    First commit.

---
+++ ConferenceCalendar/src/main/java/be/ltmartin/conferencecalendar/ConferenceCalendarApplication.java
@@ -0,0 +1,13 @@
+package be.ltmartin.conferencecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceCalendarApplication.class, args);
+    }
+
+}
---
+++ ConferenceCalendar/src/test/java/be/ltmartin/conferencecalendar/ConferenceCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ltmartin.conferencecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

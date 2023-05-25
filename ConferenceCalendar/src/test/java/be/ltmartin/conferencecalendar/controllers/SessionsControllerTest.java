709af0e51b082890940515f988cc5910c9ce0142
Leandro Tabares Martín <ltmartin1987@gmail.com>
2023-05-25 10:53:08 +0200

    Improving mock tests.

--- ConferenceCalendar/src/test/java/be/ltmartin/conferencecalendar/controllers/SessionsControllerTest.java
+++ ConferenceCalendar/src/test/java/be/ltmartin/conferencecalendar/controllers/SessionsControllerTest.java
@@ -3,18 +3,14 @@ package be.ltmartin.conferencecalendar.controllers;
 import be.ltmartin.conferencecalendar.models.Session;
 import be.ltmartin.conferencecalendar.repositories.SessionRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -31,9 +27,6 @@ import static org.mockito.Mockito.when;
 class SessionsControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
-    @InjectMocks
-    private SessionsController sessionsController;
     @MockBean
     private SessionRepository sessionRepository;
     @Autowired
@@ -82,13 +75,16 @@ class SessionsControllerTest {
     }
 
     @Test
-    void get() {
-        when(sessionRepository.getReferenceById(1L)).thenReturn(session1);
+    void get() throws Exception{
+        when(sessionRepository.getReferenceById(ArgumentMatchers.eq(1L))).thenReturn(session1);
 
-        ResponseEntity<Session> foundSession = sessionsController.get(1L);
+        ResultActions response = mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/v1/sessions/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
 
-        Assertions.assertEquals(foundSession.getBody(), session1);
-        Assertions.assertEquals(foundSession.getStatusCode(), HttpStatus.FOUND);
+        response.andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(result -> result.getResponse().getContentAsString().equals(session1.toString()));
     }
 
     @Test
